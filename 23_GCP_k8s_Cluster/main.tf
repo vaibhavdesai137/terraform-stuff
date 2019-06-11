385a61e19e91f77f2528a4eeb2c2b62dfb5474cd
vaibhavdesai137 <vaibhavdesai137@gmail.com>
2019-06-10 20:52:18 -0700

    added notes

--- 23_GCP_k8s_Cluster/main.tf
+++ 23_GCP_k8s_Cluster/main.tf
@@ -14,6 +14,28 @@
 // Download it and then set the env var accordingly
 // export GOOGLE_APPLICATION_CREDENTIALS=/path/to/service-account/key.json
 //
+// At the end of the run, the following command should be printed
+// gcloud container clusters get-credentials terraform-k8s-243400-cluster --zone us-east1-b --project terraform-k8s-243400
+// Use the above to let kubectkl point to GCP cluster instead of local
+//
+// To verify, kubectl is good:
+//
+// vaibhav$ kubectl get pods
+//      Unable to connect to the server: dial tcp 192.168.99.100:8443: connect: host is down
+//
+// vaibhav$ gcloud container clusters get-credentials terraform-k8s-243400-cluster --zone us-east1-b --project terraform-k8s-243400
+//      Fetching cluster endpoint and auth data.
+//      kubeconfig entry generated for terraform-k8s-243400-cluster.
+//
+// vaibhav$ kubectl get pods (No pods were created so nothing came back)
+//      No resources found.
+//
+// vaibhav$ kubectl get nodes (We have the 3 nodes that we created via terraform)
+//      NAME                                                  STATUS   ROLES    AGE   VERSION
+//      gke-terraform-k8s-24-terraform-k8s-24-abea7189-7w6g   Ready    <none>   44m   v1.12.8-gke.6
+//      gke-terraform-k8s-24-terraform-k8s-24-d36c2738-6sj4   Ready    <none>   43m   v1.12.8-gke.6
+//      gke-terraform-k8s-24-terraform-k8s-24-f1fd0342-1szh   Ready    <none>   43m   v1.12.8-gke.6
+//
 
 module "kubernetes" {
  
